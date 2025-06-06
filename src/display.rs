bc1fe22d68b4a6ef454aaaf0fc7c40a5a153e409
stopka <stepan_konoplev@gmx.de>
2025-06-06 15:49:39 +0200

    implemented that the table can auto adjust to the terminal size. Todos are marked red and have the x icon when due date is expired

--- src/display.rs
+++ src/display.rs
@@ -1,16 +1,17 @@
 use crate::todo::Todo;
 use crate::todo::TodoList;
-use tabled::settings::object::Columns;
-use tabled::settings::object::Object;
-use tabled::settings::Alignment;
-use tabled::settings::Width;
 use tabled::Table;
 use tabled::Tabled;
+use tabled::settings::Alignment;
 use tabled::settings::Color;
 use tabled::settings::Style;
 use tabled::settings::location::Locator;
+use tabled::settings::object::Columns;
 use tabled::settings::object::Rows;
+use tabled::settings::peaker::Priority;
 use tabled::settings::style::LineText;
+use tabled::settings::{Height, Width};
+use terminal_size::{Height as TerminalHeight, Width as TerminalWidth, terminal_size};
 
 #[derive(Tabled)]
 struct TodoDisplay {
@@ -28,7 +29,15 @@ impl TodoDisplay {
         let id = todo.get_id().to_string();
         let title = todo.get_title();
         let description = todo.get_description();
-        let finished = (if todo.is_finished() { "✅" } else { "⏳" }).to_string();
+        let finished = (if todo.is_finished() { 
+            "✅" 
+        }
+        else if todo.is_overdue() {
+            "❌"
+        }
+        else { 
+            "⏳" 
+        }).to_string();
         let priority = todo.get_priority().to_string();
         let created_at = todo
             .get_creation_date()
@@ -36,8 +45,9 @@ impl TodoDisplay {
             .to_string();
         let due_date = todo
             .get_due_date()
-            .map(|date| date.to_string())
+            .map(|date| date.format("%H:%M %d.%m.%Y").to_string())
             .unwrap_or_default();
+
         Self {
             id,
             title,
@@ -55,16 +65,33 @@ pub fn display_todo_list(todo_list: &TodoList) {
     for todo in todo_list.todos.iter() {
         todos.push(TodoDisplay::from(todo));
     }
+    let (width, height) = get_terminal_size();
     let mut table = Table::new(todos);
     table
         .with(Style::rounded())
         .with(LineText::new("Todos", Rows::first()).offset(2))
-        .modify(Columns::single(2).not(Rows::first()), Width::wrap(60))
+        .with(Width::wrap(width).priority(Priority::max(true)))
+        .with(Width::increase(width))
+        .with(Height::limit(height))
+        .with(Height::increase(height))
         .modify(Locator::content("Low"), Color::FG_GREEN)
         .modify(Locator::content("Medium"), Color::FG_YELLOW)
         .modify(Locator::content("High"), Color::FG_RED)
-        .modify(Locator::content("false"), Color::FG_RED)
-        .modify(Locator::content("true"), Color::FG_GREEN)
         .modify(Columns::single(3), Alignment::center());
+
+    for (i, todo) in todo_list.todos.iter().enumerate() {
+        let is_overdue = todo.is_overdue();
+        let finished = todo.is_finished();
+        if is_overdue && !finished {
+            table.modify(Rows::single(i + 1), Color::FG_RED);
+        }
+    }
     println!("{table}");
+}
+
+fn get_terminal_size() -> (usize, usize) {
+    let (TerminalWidth(width), TerminalHeight(height)) =
+        terminal_size().expect("failed to obtain a terminal size");
+
+    (width as usize, height as usize)
 }
